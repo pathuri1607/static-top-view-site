d3b75be489e0eff29f6afaed0888bd417d0b6ab4
Venkata Pathuri <v.pathuri@IEEE.ORG>
2019-09-16 14:55:34 -0400

    UI changes...

--- js/checkout.js
+++ js/checkout.js
@@ -1,6 +1,8 @@
 (function ($, window, document) {
     //console.log("checkout...");
 
+    $('.invalid-feedback').hide();
+
     var cartCount = (sessionStorage.length == 0) ? 0 : sessionStorage.length - 1;
     $('.cart-count').text(cartCount);
     var total = 0;
@@ -46,9 +48,71 @@
     //$('.cart-count').text($('#cartItems .cartItem').length);
 
     $(".btnRemoveItem").click(function (e) {
-        e.preventDefault();        
+        e.preventDefault();
         sessionStorage.removeItem($(e.currentTarget).data('product-id'));
         window.location.reload();
     });
+
+    $("#orderplace").click(function (e) {
+        e.preventDefault();
+
+        var isErrors = true;
+
+        $('.invalid-feedback').hide();
+
+        if($('#fname').val() === "") {
+            $('.invalid-feedback.fname').show();
+            isErrors = false;
+        } else {
+            sessionStorage.setItem("fullName", $('#fname').val());
+        }
+        if($('#email').val() === "") {
+            $('.invalid-feedback.email').show();
+            isErrors = false;
+        }
+        if($('#adr').val() === "") {
+            $('.invalid-feedback.adr').show();
+            isErrors = false;
+        }
+        if($('#city').val() === "") {
+            $('.invalid-feedback.city').show();
+            isErrors = false;
+        }
+        if($('#state').val() === "") {
+            $('.invalid-feedback.state').show();
+            isErrors = false;
+        }
+        if($('#zip').val() === "") {
+            $('.invalid-feedback.zip').show();
+            isErrors = false;
+        }
+
+        if($('#cname').val() === "") {
+            $('.invalid-feedback.cname').show();
+            isErrors = false;
+        }
+        if($('#ccnum').val() === "") {
+            $('.invalid-feedback.ccnum').show();
+            isErrors = false;
+        }
+        if($('#expmonth').val() === "") {
+            $('.invalid-feedback.expmonth').show();
+            isErrors = false;
+        }
+        if($('#expyear').val() === "") {
+            $('.invalid-feedback.expyear').show();
+            isErrors = false;
+        }
+        if($('#cvv').val() === "") {
+            $('.invalid-feedback.cvv').show();
+            isErrors = false;
+        }
+
+        if(isErrors) {
+            window.location.href = "successMsg.html";
+        } else {
+            $("html, body").animate({ scrollTop: 0 }, "slow");
+        }
+    })
 
 }(window.jQuery, window, document));
